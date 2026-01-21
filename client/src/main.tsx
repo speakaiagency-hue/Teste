4bd220d3d28cc24d20823d046a5c6461de067fc5
speakaiagency-hue <speakai.agency@gmail.com>
2026-01-21 00:54:05 -0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Pega o elemento #root do index.html e renderiza o App dentro dele
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error("Elemento #root não encontrado no index.html");
-}
+createRoot(document.getElementById("root")!).render(<App />);
